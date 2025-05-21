b4b3e46c0186402309ac7203cd0fb3ba933422a5
Abhinav Kuruvila Joseph <62714538+IsotoxalDev@users.noreply.github.com>
2025-05-21 12:22:05 +0530

    Helix

--- home.nix
+++ home.nix
@@ -110,5 +110,23 @@
     };
   };
 
+
+  # Helix
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "everblush";
+      editor = {
+        line-number = "relative";
+        true-color = true;
+        mouse=false;
+        middle-click-paste = false;
+        shell = ["zsh"];
+        cursor-shape.insert = "bar";
+        whitespace.render.newline = "all";
+      };
+    };
+  };
+
   programs.home-manager.enable = true;
 }
